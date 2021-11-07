c76f3fb0c11c2e97e28416deed09ecdbfe979705
Greg Douglas <greg@storyblocks.com>
2021-11-07 01:19:02 -0400

    timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,14 @@
 import styles from './Timeline.module.css';
 
 export default function Timeline() {
-  return <div className={styles.timeline}></div>;
+  return (
+    <div className={styles.timeline}>
+      <div className={styles.scrollContainer}>
+        <div className={styles.track}></div>
+        <div className={styles.track}></div>
+        <div className={styles.track}></div>
+        <div className={styles.track}></div>
+      </div>
+    </div>
+  );
 }
